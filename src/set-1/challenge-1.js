6230268f7d6211a6147a1eee51b3f9f784650e84
Vince Falconi <vince@tattooed.dev>
2023-08-10 20:00:56 -0500

    clean up set 1 challenge 1

--- src/set-1/challenge-1.js
+++ src/set-1/challenge-1.js
@@ -1,18 +1,28 @@
+const url = 'https://www.cryptopals.com/sets/1/challenges/1';
+const description = 'Convert hex to base64';
 const input = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d';
 
 const string2chunk = (str, length = 2) => {
 	return str.match(new RegExp(`.{1,${length}}`, 'g'));
 };
 
-const hex2base64 = (hex) => {
+const decodeHex = (hex) => {
 	const decimalArray = string2chunk(hex).map(hstr => parseInt(hstr, 16));
-	const readable = String.fromCharCode(...decimalArray);
-	return Buffer.from(readable).toString('base64');
+	return Buffer.from(decimalArray).toString('utf8');
+}
+
+const hex2base64 = (hex) => {
+	return Buffer.from(decodeHex(hex)).toString('base64');
 };
 
 module.exports = {
+	url,
+	description,
 	input,
-	hex2base64,
-	string2chunk,
+	answer: hex2base64(input),
+	tools: {
+		hex2base64,
+		decodeHex,
+		string2chunk,
+	},
 };
-module.exports.answer = hex2base64(input);
