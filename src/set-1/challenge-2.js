de4f23c73de26390191401e1b58074884633393b
Vince Falconi <vince@tattooed.dev>
2023-08-10 20:02:43 -0500

    solve set 1 challenge 2

---
+++ src/set-1/challenge-2.js
@@ -0,0 +1,29 @@
+const url = 'https://www.cryptopals.com/sets/1/challenges/2';
+const description = 'Fixed XOR';
+const input = [
+	'1c0111001f010100061a024b53535009181c',
+	'686974207468652062756c6c277320657965',
+];
+const { decodeHex } = require('./challenge-1').tools;
+
+const fixedXor = (a, b) => {
+	const p = [...Buffer.from(decodeHex(a)).toJSON().data];
+	const q = [...Buffer.from(decodeHex(b)).toJSON().data];
+	const results = [];
+
+	for (let i = 0; i < p.length; i++) {
+		results.push(p[i] ^ q[i]);
+	}
+
+	return Buffer.from(results).toString('hex');
+};
+
+module.exports = {
+	url,
+	description,
+	input,
+	answer: fixedXor(...input),
+	tools: {
+		fixedXor,
+	},
+};
--- src/set-1/index.js
+++ src/set-1/index.js
@@ -1,5 +1,7 @@
 const challange1 = require('./challenge-1');
+const challange2 = require('./challenge-2');
 
 module.exports = [
 	challange1,
+	challange2,
 ];
